e6e5f9d3e23e5f1205c784330da736e99b603925
Carlos Martins <csmartins.ufrj@gmail.com>
2017-10-19 17:21:59 -0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ echo "<h1>Openshift Workshop o/</h1> ";
 echo "<h2> Ip do pod </h2>";
 echo $_SERVER['SERVER_ADDR'];
 echo "<br><hr>";
-echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
+echo "<h2>Cidades cadastradas no nosso Banco de Dados:</h2>";
 $conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
